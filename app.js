e0713d335d58b5ab21b3efaca457b6efa2c33cb6
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-03-31 18:25:00 +0200

    creazione cartella public

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
+const articoli = [
+    {
+        titolo:"",
+        
+    },
+]
 
 app.get('/' , (req, res) => {
     res.send("bevenuti nella home page")
